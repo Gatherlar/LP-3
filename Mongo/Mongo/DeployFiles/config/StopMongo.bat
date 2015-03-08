88b0df7ce58dc2434bf21d85e790579bb75db144
Mr Mat <mat.hayes@outlook.com>
2015-03-08 21:16:37 +0000

    Added more Mongo pages for the Start/Stop in code

---
+++ Mongo/Mongo/DeployFiles/config/StopMongo.bat
@@ -0,0 +1,2 @@
+net stop mdb27017
+pause
---
+++ Mongo/Mongo/DeployFiles/config/serviceRemoveMongo.bat
@@ -0,0 +1,4 @@
+cd "C:\Program Files\MongoDB 2.6 Standard\bin\"
+mongod -f "C:\GitHub\Survivor\SL\DeployFiles\Config\mongodb.conf" --remove --serviceName mdb27017 --serviceDisplayName "MongoDB Server Instance 27017" --serviceDescription "MongoDB Server Instance running on 27017" 
+
+pause
