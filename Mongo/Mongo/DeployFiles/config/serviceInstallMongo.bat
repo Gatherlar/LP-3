0460d4f6b3b3865cec4cf37f908cec5a837ae0f4
Mr Mat <mat.hayes@outlook.com>
2015-03-27 07:23:58 +0000

    Updated the service page and added a new item in querying mongo

--- Mongo/Mongo/DeployFiles/config/serviceInstallMongo.bat
+++ Mongo/Mongo/DeployFiles/config/serviceInstallMongo.bat
@@ -1,4 +1,4 @@
-cd "C:\Program Files\MongoDB 2.6 Standard\bin\"
-mongod -f "C:\GitHub\Survivor\SL\DeployFiles\Config\mongodb.conf" -install -serviceName mdb27017 -serviceDisplayName "MongoDB Server Instance 27017" -serviceDescription "MongoDB Server Instance running on 27017"
+cd "C:\Program Files\MongoDB\Server\3.0\bin" 
+mongod -f "C:\GitHub\Survivor\SL\DeployFiles\Config\mongodb.conf" -install -serviceName mongoDB27017 -serviceDisplayName "MongoDB Server Instance port 27017" -serviceDescription "MongoDB Server Instance running on 27017"
 
 pause
