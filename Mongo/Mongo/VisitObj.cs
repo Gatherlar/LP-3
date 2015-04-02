496b4b8cef4991cc9dcd6e48cd1ece8c6209d539
Mr Mat <mat.hayes@outlook.com>
2015-04-02 09:06:16 +0100

    Added visit objects for relational links

---
+++ Mongo/Mongo/VisitObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace Mongo
+{
+    public class VisitObj
+    {
+
+        [BsonId]
+        public ObjectId _id;
+
+        public ObjectId idClient;
+        public ObjectId idCarer;
+        public DateTime VisitStart;
+        public DateTime VisitEnd;
+        public string ClientName;
+        public string CareName;
+
+        [BsonIgnore]
+        public ClientObj client;
+        [BsonIgnore]
+        public CarerObj carer;
+
+
+    }
+}
---
+++ Mongo/Mongo/VisitsObj.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Mongo
+{
+    class VisitsObj
+    {
+        private MongoClient _mongo;
+        private MongoServer _server;
+        private MongoDatabase _database;
+        private MongoCollection<VisitObj> _visit;
+
+
+        public VisitObj(string visitID)
+        {
+            _mongo = new MongoClient();
+            _server = _mongo.GetServer();
+            _database = _server.GetDatabase("CMdb");
+            // Using generics here means that we dont not need to use "find as" when accessing data in the collection
+            _visit = _database.GetCollection<VisitObj> ("Visits");
+            Populate(visitID);
+        }
+
+        private VisitObj Populate(string visitID)
+        {
+           var visit =  _visit.FindOneByIdAs<VisitObj>(ObjectId.Parse(visitID));
+           return visit;
+
+        }
+
+
+    }
+}
+
+
+
+
